bf996a2df1b5071a5f3b9ae6965e7a1013096b78
aviorn36 <aviorn36@gmail.com>
2024-02-10 12:58:33 +0530

    warning fix

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -30,7 +30,12 @@ const NavBar = () => {
           {socialLinks.map(({ id, href, icon }) => {
             return (
               <li key={id}>
-                <a href={href} target="_blank" className="nav-icon">
+                <a
+                  href={href}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="nav-icon"
+                >
                   <i className={icon}></i>
                 </a>
               </li>
